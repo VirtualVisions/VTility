43262997deaa29d697d1cf1989a20bc4d62b166b
Vowgan <47050502+Vowgan@users.noreply.github.com>
2026-06-07 17:13:20 -0500

    Added extensions

--- Packages/com.virtual-visions.vtility/Runtime/SettableBool.cs
+++ Packages/com.virtual-visions.vtility/Runtime/SettableBool.cs
@@ -6,4 +6,12 @@ namespace VirtualVisions.VTility
         False,
         True
     }
+    
+    public static class SettableBoolExtensions
+    {
+        public static bool IsSet(this SettableBool settable) => settable != SettableBool.Unset;
+        public static bool IsFalse(this SettableBool settable) => settable == SettableBool.False;
+        public static bool IsTrue(this SettableBool settable) => settable == SettableBool.True;
+    }
+    
 }
