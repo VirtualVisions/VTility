0e45114e34e2fe84624d325a58451c3a71f27415
Vowgan <47050502+Vowgan@users.noreply.github.com>
2026-06-12 15:10:20 -0500

    Added ListView structure.

---
+++ Packages/com.virtual-visions.vtility/Runtime/UI/ListView/CenteringList.cs
@@ -0,0 +1,128 @@
+﻿using UnityEngine;
+using VRC.SDK3.Components;
+
+namespace VirtualVisions.Varcade
+{
+    public class CenteringList : UdonListView
+    {
+
+        [SerializeField] protected float _itemSize = 50;
+        [SerializeField] protected float _spacingSize = 15;
+        [SerializeField] private float _tweenDuration = 1;
+        [SerializeField] private VRCTweenEase _tweenEase = VRCTweenEase.OutCubic;
+
+        private VRCTweenHandle _currentTween;
+
+        protected float ContainerSize => GetItemPlacement(ItemCount);
+        protected float ItemHalfSize => _itemSize / 2f;
+        protected float FullItemSize => _itemSize + _spacingSize;
+
+
+        private float GetItemPlacement(int index)
+        {
+            return ((index * _itemSize) + ((index - 1) * _spacingSize) + ItemHalfSize);
+        }
+
+        private Vector2 GetLayoutPosition(int index) 
+        {
+            switch (_direction)
+            {
+                default:
+                case LayoutDirection.Column:
+                    return new Vector2(0, -GetItemPlacement(index));
+                case LayoutDirection.Row:
+                    return new Vector2(GetItemPlacement(index), 0);
+            }
+        }
+
+        private void OnEnable()
+        {
+            _itemContainer.anchoredPosition = -GetLayoutPosition(SelectedIndex);
+            RefreshVisibility();
+        }
+
+        private void Update()
+        {
+            if (_currentTween.IsActive)
+            {
+                RefreshVisibility();
+            }
+        }
+
+        public void _TweenToCurrent()
+        {
+            if (_currentTween.IsActive) _currentTween.Kill();
+
+            _currentTween = _itemContainer.TweenAnchorPos(
+                -GetLayoutPosition(SelectedIndex),
+                _tweenDuration,
+                _tweenEase);
+        }
+
+        public void _NavigateNext(Vector2Int direction)
+        {
+            switch (_direction)
+            {
+                default:
+                case LayoutDirection.Column:
+                    if(direction.y == 1) _SelectPrevious();
+                    if(direction.y == -1) _SelectNext();
+                    break;
+                case LayoutDirection.Row:
+                    if(direction.x == 1) _SelectNext();
+                    if(direction.x == -1) _SelectPrevious();
+                    break;
+            }
+        }
+        
+        public void _SelectPrevious() => _SetIndex(SelectedIndex - 1);
+        public void _SelectNext() => _SetIndex(SelectedIndex + 1);
+
+
+        public override void _SetIndex(int index)
+        {
+            base._SetIndex(index);
+            _TweenToCurrent();
+        }
+
+        protected override RectTransform CreateItem()
+        {
+            RectTransform item = base.CreateItem();
+            
+            switch (_direction)
+            {
+                default:
+                case LayoutDirection.Column:
+                    item.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, _itemSize);
+                    _itemContainer.sizeDelta = new Vector2(0, ContainerSize);
+                    break;
+                case LayoutDirection.Row:
+                    item.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, _itemSize);
+                    _itemContainer.sizeDelta = new Vector2(ContainerSize, 0);
+                    break;
+            }
+            
+            return item;
+        }
+
+        private void RefreshVisibility()
+        {
+            for (int i = 0; i < ItemCount; i++)
+            {
+                bool visible = IsIndexVisible(i, FullItemSize, ItemHalfSize);
+                bool isActive = _activeItemKeys.ContainsKey(i);
+
+                if (visible == isActive) continue;
+                if (visible)
+                {
+                    RectTransform item = GetItem(i);
+                    item.anchoredPosition = GetLayoutPosition(i);
+                }
+                else
+                {
+                    ReleaseItem(i);
+                }
+            }
+        }
+    }
+}
---
+++ Packages/com.virtual-visions.vtility/Runtime/UI/ListView/UdonListView.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using UdonSharp;
+using UnityEngine;
+using VirtualVisions.VTility;
+using VRC.SDK3.Data;
+
+namespace VirtualVisions.Varcade
+{
+
+    public enum LayoutDirection
+    {
+        Column,
+        Row,
+    }
+    
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public abstract class UdonListView : UdonSharpBehaviour
+    {
+        /// <summary>
+        /// Called when data is bound to a specific item.
+        /// Listeners must contain a [RectTransform: item] and [Int: index] variable within a DataList.
+        /// </summary>
+        public UdonAction OnBindItem => (UdonAction)(_onBindItem != null ? _onBindItem : _onBindItem = UdonAction.Create());
+        private DataDictionary _onBindItem;
+
+        /// <summary>
+        /// Called when an item is focused in the list.
+        /// Listeners must contain a [RectTransform: item] variable.
+        /// </summary>
+        public UdonAction OnItemSelected => (UdonAction)(_onItemSelected != null ? _onItemSelected : _onItemSelected = UdonAction.Create());
+        private DataDictionary _onItemSelected;
+
+        /// <summary>
+        /// Called when an item is selected and used.
+        /// Listeners must contain a [RectTransform: item] variable.
+        /// </summary>
+        public UdonAction OnItemUsed => (UdonAction)(_onItemUsed != null ? _onItemUsed : _onItemUsed = UdonAction.Create());
+        private DataDictionary _onItemUsed;
+
+        public DataList ItemSource { get; protected set; }
+        public int SelectedIndex { get; private set; }
+
+
+        [SerializeField] protected RectTransform _itemPrefab;
+        [SerializeField] protected RectTransform _itemContainer;
+        [SerializeField] protected RectTransform _viewport;
+        [SerializeField] protected LayoutDirection _direction;
+        
+        protected DataDictionary _activeItemKeys = new DataDictionary();
+        protected DataList _activeItems = new DataList();
+        protected DataList _inactiveItems = new DataList();
+        
+        protected int ItemCount => ItemSource != null ? ItemSource.Count : 0;
+
+        
+        public virtual void _SetIndex(int index)
+        {
+            SelectedIndex = Mathf.Clamp(index, 0, ItemSource.Count - 1);
+        }
+
+        public virtual void _SetItemSource(DataList list)
+        {
+            ItemSource = list;
+        }
+
+        protected virtual RectTransform GetItem(int index)
+        {
+            if (_activeItemKeys.TryGetValue(index, TokenType.Reference, out DataToken value))
+            {
+                return (RectTransform)value.Reference;
+            }
+
+            RectTransform item;
+            if (_inactiveItems.Count > 0)
+            {
+                item = (RectTransform)_inactiveItems[0].Reference;
+            }
+            else
+            {
+                item = CreateItem();
+            }
+            
+            _inactiveItems.Remove(item);
+            _activeItems.Add(item);
+            _activeItemKeys[index] = item;
+            BindItem(item, index);
+
+            return item;
+        }
+
+        protected virtual RectTransform CreateItem()
+        {
+            RectTransform obj = (RectTransform)Instantiate(_itemPrefab.gameObject, _itemContainer).transform;
+            _inactiveItems.Add(obj);
+            return obj;
+        }
+
+        protected void BindItem(RectTransform item, int index)
+        {
+            item.gameObject.SetActive(true);
+
+            DataList bindParams = new DataList();
+            bindParams.Add(item);
+            bindParams.Add(index);
+            OnBindItem._Invoke(bindParams);
+        }
+
+        protected void ReleaseItem(int index)
+        {
+            if (!_activeItemKeys.TryGetValue(index, TokenType.Reference, out DataToken value))
+            {
+                Debug.LogWarning($"Item index {index} is not currently active.", this);
+                return;
+            }
+
+            RectTransform item = (RectTransform)value.Reference;
+            item.gameObject.SetActive(false);
+            
+            _inactiveItems.Add(item);
+            _activeItems.Remove(item);
+            _activeItemKeys.Remove(index);
+        }
+
+        protected bool IsIndexVisible(int index, float itemSize, float offset = 0)
+        {
+            // The extra add/subtract at the end of these two is for
+            // partial visibility when an item is coming into view.
+            float itemStart = (index * itemSize) + itemSize + offset;
+            float itemEnd = ((index + 1) * itemSize) - itemSize - offset;
+
+            float containerOffset;
+            switch (_direction)
+            {
+                default:
+                case LayoutDirection.Column:
+                    containerOffset = _itemContainer.anchoredPosition.y;
+                    break;
+                case LayoutDirection.Row:
+                    containerOffset = -_itemContainer.anchoredPosition.x;
+                    break;
+            }
+            
+            float itemStartViewport = itemStart - containerOffset;
+            float itemEndViewport = itemEnd - containerOffset;
+
+            float viewportStart;
+            float viewportEnd;
+            
+            switch (_direction)
+            {
+                default:
+                case LayoutDirection.Column:
+                    viewportStart = _viewport.rect.yMin;
+                    viewportEnd = _viewport.rect.yMax;
+                    break;
+                case LayoutDirection.Row:
+                    viewportStart = _viewport.rect.xMin;
+                    viewportEnd = _viewport.rect.xMax;
+                    break;
+            }
+
+            return itemStartViewport > viewportStart &&
+                   itemEndViewport < viewportEnd;
+        }
+
+    }
+}
