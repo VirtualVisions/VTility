a390a6db5a5fa331269775b47c8991a65e3dd3f7
Vowgan <47050502+Vowgan@users.noreply.github.com>
2026-06-17 15:49:12 -0500

    Added VE Style extensions

---
+++ Packages/com.virtual-visions.vtility/Editor/Extensions/ElementStyleExtensions.cs
@@ -0,0 +1,29 @@
+using UnityEngine.UIElements;
+
+namespace VirtualVisions.VTility.Editor
+{
+    public static class ElementStyleExtensions
+    {
+
+        public static bool StyleDisplay(this VisualElement element)
+        {
+            return element.style.display == DisplayStyle.Flex;
+        }
+
+        public static void StyleDisplay(this VisualElement element, bool value)
+        {
+            element.style.display = value ? DisplayStyle.Flex : DisplayStyle.None;
+        }
+
+        public static bool StyleVisible(this VisualElement element)
+        {
+            return element.style.visibility == Visibility.Visible;
+        }
+
+        public static void StyleVisible(this VisualElement element, bool value)
+        {
+            element.style.visibility = value ? Visibility.Visible : Visibility.Hidden;
+        }
+        
+    }
+}
