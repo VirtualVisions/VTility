bd05728bc07dab16e48952bdfb2c9040895e9434
Vowgan <47050502+Vowgan@users.noreply.github.com>
2026-06-29 16:17:17 -0500

    Added CopyEntry for datadictionaries

---
+++ Packages/com.virtual-visions.vtility/Runtime/Extensions/DataDictionaryExtensions.cs
@@ -0,0 +1,27 @@
+﻿using VRC.SDK3.Data;
+
+namespace VirtualVisions.VTility
+{
+    public static class DataDictionaryExtensions
+    {
+        public static void CopyEntry(
+            this DataDictionary dict,
+            DataDictionary from,
+            TokenType type,
+            DataToken key)
+        {
+            if (from.TryGetValue(key, type, out DataToken value)) dict[key] = value;
+        }
+        
+        public static void CopyEntry(
+            this DataDictionary dict,
+            DataDictionary from,
+            TokenType type,
+            DataToken key,
+            DataToken fallback)
+        {
+            if (from.TryGetValue(key, type, out DataToken value)) dict[key] = value;
+            else dict[key] = fallback;
+        }
+    }
+}
